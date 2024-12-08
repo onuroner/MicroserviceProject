cfe839df97ca3d607624c1fa8d42db7a8af9cf00
Onur Öner <onuroner4054@gmail.com>
2024-12-08 17:41:58 +0300

    GetCoursesByUserId added

--- UdemyNewMicroservice.Catalog.API/Features/Courses/CourseEndpointExtension.cs
+++ UdemyNewMicroservice.Catalog.API/Features/Courses/CourseEndpointExtension.cs
@@ -7,6 +7,7 @@ using UdemyNewMicroservice.Catalog.API.Features.Categories.GetById;
 using UdemyNewMicroservice.Catalog.API.Features.Courses.Create;
 using UdemyNewMicroservice.Catalog.API.Features.Courses.Delete;
 using UdemyNewMicroservice.Catalog.API.Features.Courses.GetAll;
+using UdemyNewMicroservice.Catalog.API.Features.Courses.GetAllByUserId;
 using UdemyNewMicroservice.Catalog.API.Features.Courses.GetById;
 using UdemyNewMicroservice.Catalog.API.Features.Courses.Update;
 
@@ -21,7 +22,8 @@ namespace UdemyNewMicroservice.Catalog.API.Features.Courses
                 .GetAllCoursesGroupItemEndpoint()
                 .GetCourseByIdGroupItemEndpoint()
                 .UpdateCourseGroupItemEndpoint()
-                .DeleteCourseGroupItemEndpoint();
+                .DeleteCourseGroupItemEndpoint()
+                .GetCoursesByUserIdGroupItemEndpoint();
                 
         }
     }
---
+++ UdemyNewMicroservice.Catalog.API/Features/Courses/GetAllByUserId/GetCoursesByUserIdEndpoint.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using MediatR;
+using MicroserviceProject.Shared;
+using MicroserviceProject.Shared.Extensions;
+using Microsoft.EntityFrameworkCore;
+using UdemyNewMicroservice.Catalog.API.Features.Courses.Dto;
+using UdemyNewMicroservice.Catalog.API.Repositories;
+
+namespace UdemyNewMicroservice.Catalog.API.Features.Courses.GetAllByUserId
+{
+    public record GetCoursesByUserIdQuery(Guid UserId) : IRequest<ServiceResult<List<CourseDto>>>;
+
+    public class GetCoursesByUserIdQueryHandler(AppDbContext context, IMapper mapper) : IRequestHandler<GetCoursesByUserIdQuery, ServiceResult<List<CourseDto>>>
+    {
+        public async Task<ServiceResult<List<CourseDto>>> Handle(GetCoursesByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            var courses = await context.Courses.Where(x => x.UserId == request.UserId).ToListAsync();
+            if (courses is null)
+            {
+                return ServiceResult<List<CourseDto>>.Error("User has not any courses", System.Net.HttpStatusCode.NotFound);
+            }
+            
+            var categories = await context.Categories.ToListAsync();
+            foreach (var course in courses)
+            {
+                course.Category = categories.Where(x => x.Id == course.CategoryId).First();
+            }
+            var mappedCourses = mapper.Map<List<CourseDto>>(courses);
+
+            return ServiceResult<List<CourseDto>>.SucccessAsOk(mappedCourses);
+        }
+    }
+
+    public static class GetCoursesByUserIdEndpoint
+    {
+        public static RouteGroupBuilder GetCoursesByUserIdGroupItemEndpoint(this RouteGroupBuilder group)
+        {
+            group.MapGet("/user/{userId:guid}", async (Guid UserId, IMediator mediator) =>
+            {
+                var result = await mediator.Send(new GetCoursesByUserIdQuery(UserId));
+                return result.ToGenericResult();
+            }).WithName("GetCoursesByUserId");
+
+            return group;
+        }
+    }
+}
